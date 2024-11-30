df1e887d90e75ce8f283d675a82bf8d34407239e
DEBRIAT AMAURY <tontonmox@gmail.com>
2024-11-30 21:50:57 +0100

    Test

---
+++ utils/utils.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"bufio"
+	"os"
+)
+
+func Check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func ReadInput(day string) []string {
+	path := "./inputs/" + day
+	file, err := os.Open(path)
+
+	Check(err)
+
+	var fileText []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fileText = append(fileText, scanner.Text())
+	}
+
+	return fileText
+}
