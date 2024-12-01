1e6a34f780bbc0331c7247a6a0d813401d0b5f7b
DEBRIAT AMAURY <tontonmox@gmail.com>
2024-12-01 07:10:51 +0100

    Ajout du day 1

--- puzzles/day1.go
+++ puzzles/day1.go
@@ -3,11 +3,13 @@ package puzzles
 import (
 	"fmt"
 	"go_2024/utils"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func Day1() {
 	part1()
-	part2()
 }
 
 func part1() {
@@ -15,17 +17,55 @@ func part1() {
 
 	inputs := utils.ReadInputInSlice("1")
 
+	var sliceA []int
+	var sliceB []int
+
 	for _, line := range inputs {
-		fmt.Println(line)
+		//Get each of the 2 values as an int and store them into a slice
+		values := strings.Split(line, "   ")
+
+		//Valeur 1
+		valA, err := strconv.Atoi(values[0])
+		if err != nil {
+			fmt.Println("Valeur non numérique suite au slice de  " + line)
+			panic(err)
+		}
+		sliceA = append(sliceA, valA)
+
+		//Valeur 2
+		valB, err := strconv.Atoi(values[1])
+		if err != nil {
+			fmt.Println("Valeur non numérique suite au slice de  " + line)
+			panic(err)
+		}
+		sliceB = append(sliceB, valB)
 	}
+
+	//Tri des deux slice pour faire la comparaison
+	sort.Ints(sliceA)
+	sort.Ints(sliceB)
+
+	res_part1 := 0
+	//Parcours des 2 slices en simultané et calcul du résultat
+	for i, a := range sliceA {
+		res_part1 += utils.AbsInt(a - sliceB[i])
+	}
+	fmt.Printf("Le résultat pour la partie 1 est : %d\n", res_part1)
+	part2(sliceA, sliceB)
 }
 
-func part2() {
+func part2(a []int, b []int) {
 	fmt.Println("Day 1 part 2")
 
-	inputs := utils.ReadInputInSlice("1")
-
-	for _, line := range inputs {
-		fmt.Println(line)
+	res_part2 := 0
+	for _, a := range a {
+		nbOcc := utils.CountInSlice(
+			b,
+			func(x int) bool {
+				return x == a
+			},
+		)
+		res_part2 += a * nbOcc
 	}
+	fmt.Printf("Le résultat pour la partie 2 est : %d\n", res_part2)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -25,3 +25,22 @@ func ReadInputInSlice(day string) []string {
 
 	return fileText
 }
+
+// Fonction valeur absolue pour des int
+func AbsInt(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// Fonction qui compte les éléments dans une slice répondant à une condition
+func CountInSlice[T any](slice []T, f func(T) bool) int {
+	count := 0
+	for _, s := range slice {
+		if f(s) {
+			count++
+		}
+	}
+	return count
+}
