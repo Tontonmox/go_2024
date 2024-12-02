59dc07d336d32cb3e03e1c03a00cfa339c0f5dcd
DEBRIAT AMAURY <tontonmox@gmail.com>
2024-12-02 22:32:09 +0100

    Test

--- puzzles/day2.go
+++ puzzles/day2.go
@@ -31,7 +31,7 @@ func Day2(isTest bool) {
 			nbSafe1++
 			nbSafe2++
 		} else {
-			//Part 2 : if there was an error, make 4 different lists, by removing each time one of the two numbers creating the errors, on each direction
+			//Part 2 CRADO : if there was an error, make 4 different lists, by removing each time one of the two numbers creating the errors, on each direction
 			//If one on these lists is safe then result is ok for part 2
 			var newList1 []int
 			newList1 = append(newList1, levels[:errorIndex]...)
